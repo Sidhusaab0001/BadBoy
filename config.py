32c6e17b16d4bf4d4dde91c985de2bd94d8f7acf
ɴᴏᴏʙ sɪᴅʜᴜ <132650820+Sidhusaab0001@users.noreply.github.com>
2024-06-03 23:21:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_USERNAME = getenv("BOT_USERNAME", "MissBroken_Bot")
 
 # Don't Add style font
-BOT_USERNAME2 = getenv("BOT_USERNAME2", "miss Broken")
+BOT_USERNAME2 = getenv("BOT_USERNAME2", "SUKOON_MUSIC_RBOT")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
@@ -27,7 +27,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002043538118"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001934011554")
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
@@ -40,15 +40,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/badmunda011/PBXMUSIC",
+    "https://github.com/TrickBySaqib/BadBoy",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/HEROKUBIN_01")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/THE_PUNJABI_BANDE")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/STYLISH_FOUT_CHANNEL")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/OWNER_TBS")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
